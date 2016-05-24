338e0d68fa510b9ab3ef27de7378ce49e793833e
Ted Sappington <tedsappington@gmail.com>
2016-05-23 18:33:38 -0700

    Assignment 28 Fixed (schema.rb)

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,11 @@ ActiveRecord::Schema.define(version: 20160522232030) do
   create_table "answers", force: :cascade do |t|
     t.text     "body"
     t.integer  "question_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
