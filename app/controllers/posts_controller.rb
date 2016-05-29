04b8c0887ffb1c25e85d19657f3b8234c3e99d5c
Ted Sappington <tedsappington@gmail.com>
2016-05-29 13:16:48 -0700

    Checkpoint 33 More CRUD: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -31,6 +32,33 @@ class PostsController < ApplicationController
        render :new
      end
    end
+
+   def update
+      @post = Post.find(params[:id])
+      @post.title = params[:post][:title]
+      @post.body = params[:post][:body]
+
+      if @post.save
+        flash[:notice] = "Post was updated successfully."
+        redirect_to @post
+      else
+        flash.now[:alert] = "There was an error saving the post. Please try again."
+        render :edit
+      end
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+
+    # #8
+      if @post.destroy
+        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+        redirect_to posts_path
+      else
+        flash.now[:alert] = "There was an error deleting the post."
+        render :show
+      end
+    end
 
 
 end
