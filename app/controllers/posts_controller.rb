d3425c9ca9e053212d2d0ac58af2ab233f172795
Ted Sappington <tedsappington@gmail.com>
2016-05-28 17:45:25 -0700

    Assignment 31 completed (for real)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.first.update!( title: "SPAM" )
+    @posts.each_with_index do |post, x|
+        post.update!( title: "SPAM" ) if (x % 5) == 0
+    end
+
   end
 
   def show
