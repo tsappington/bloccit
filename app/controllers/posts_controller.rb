727c56ff292d047a306229017fd893028621c7e1
Ted Sappington <tedsappington@gmail.com>
2016-06-11 17:58:19 -0700

    Checkpoint 39 Posts and Users: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :require_sign_in, except: :show
+
   def show
     @post = Post.find(params[:id])
   end
@@ -14,20 +16,14 @@ class PostsController < ApplicationController
   end
 
   def create
- # #9
-     @post = Post.new
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
      @topic = Topic.find(params[:topic_id])
-     @post.topic = @topic
+     @post = @topic.posts.build(post_params)
+     @post.user = current_user
 
- # #10
      if @post.save
- # #11
        flash[:notice] = "Post was saved successfully."
        redirect_to [@topic, @post]
      else
- # #12
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :new
      end
@@ -35,8 +31,7 @@ class PostsController < ApplicationController
 
    def update
       @post = Post.find(params[:id])
-      @post.title = params[:post][:title]
-      @post.body = params[:post][:body]
+      @post.assign_attributes(post_params)
 
       if @post.save
         flash[:notice] = "Post was updated successfully."
@@ -50,7 +45,6 @@ class PostsController < ApplicationController
     def destroy
       @post = Post.find(params[:id])
 
-    # #8
       if @post.destroy
         flash[:notice] = "\"#{@post.title}\" was deleted successfully."
         redirect_to @post.topic
@@ -60,5 +54,9 @@ class PostsController < ApplicationController
       end
     end
 
+    private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -12,6 +23,7 @@ topics = Topic.all
  # Create Posts
  50.times do
    Post.create!(
+     user:  users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -28,7 +40,14 @@ topics = Topic.all
    )
  end
 
+  user = User.first
+  user.update_attributes!(
+    email: 'tedsappington@gmail.com',
+    password: 'helloworld'
+  )
+
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
