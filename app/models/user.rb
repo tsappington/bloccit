f611bcd6109b8982351596b382adeb90e541215a
Ted Sappington <tedsappington@gmail.com>
2016-06-12 17:30:16 -0700

    Checkpoint 40 Authorization: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
@@ -13,5 +14,7 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611211220) do
+ActiveRecord::Schema.define(version: 20160612210302) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160611211220) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
