6f140072e051745db95e3d59999e79eecdeac0ac
Ted Sappington <tedsappington@gmail.com>
2016-06-25 17:06:12 -0700

    Checkpoint 42 Labels

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts
   has_many :posts, dependent: :destroy
+
+  has_many :labelings, as: :labelable
+  has_many :labels, through: :labelings
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: name, description: description) }
 
    it { is_expected.to have_many(:posts) }
-
-# #1
+   it { is_expected.to have_many(:labelings) }
+   it { is_expected.to have_many(:labels).through(:labelings) }
+   
   describe "attributes" do
     it "has name, description, and public attributes" do
       expect(topic).to have_attributes(name: name, description: description, public: public)
