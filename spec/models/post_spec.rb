d40dfe7125a5943fcf3870c461293aace770a8b9
Ted Sappington <tedsappington@gmail.com>
2016-07-31 19:11:24 -0700

    Checkpoint 45 Public Profiles: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Post, type: :model do
   let(:description) { RandomData.random_paragraph }
   let(:title) { RandomData.random_sentence }
   let(:body) { RandomData.random_paragraph }
-  let(:topic) { Topic.create!(name: name, description: description) }
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:post) { topic.posts.create!(title: title, body: body, user: user) }
+  let(:topic) { create(:topic) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
 
   it { is_expected.to have_many(:labelings) }
   it { is_expected.to have_many(:labels).through(:labelings) }
@@ -29,8 +29,8 @@ RSpec.describe Post, type: :model do
 
   describe  "attributes" do
     it "has a title, body, and user attribute" do
-       expect(post).to have_attributes(title: title, body: body, user: user)    end
-  end
+       expect(post).to have_attributes(title: post.title, body: post.body)
+    end
 
   describe "voting" do
     before do
@@ -78,4 +78,6 @@ RSpec.describe Post, type: :model do
     end
 
   end
+end
+
 end
