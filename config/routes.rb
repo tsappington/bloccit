183a9f6865754348fd5ebdd8066f1ffd33b6fe9b
Ted Sappington <tedsappington@gmail.com>
2016-11-25 09:03:09 -0800

    Assignment 49 completed

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:edit, :new]
+      resources :topics, except: [:edit, :new] do
+        resources :posts, only: [:create]
+      end
+      resources :posts, only: [:update, :destroy]
      end
   end
 
